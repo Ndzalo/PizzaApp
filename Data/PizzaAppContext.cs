1944572ded4d8b8e890906849028702e220ddb59
Ndzalo <ndalomaluks@gmail.com>
2025-02-27 14:33:18 +0200

    Add project files.

---
+++ Data/PizzaAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Model;
+
+namespace PizzaApp.Data
+{
+    public class PizzaAppContext : DbContext
+    {
+        public PizzaAppContext (DbContextOptions<PizzaAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PizzaApp.Model.Pizza> Pizza { get; set; } = default!;
+    }
+}
---
+++ Model/Pizza.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaApp.Model
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        [Column(TypeName = "decimal(10,2)")]  
+        public decimal Price { get; set; }
+    }
+}
